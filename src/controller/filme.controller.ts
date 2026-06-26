fad82ea7f0e6a449be9934193a1f755df910dd53
Gustavo Daniel Vitor <d2021020134@unifei.edu.br>
2026-06-26 19:53:58 -0300

    feat(controller): adiciona métodos update e delete em filme.controller

--- src/controller/filme.controller.ts
+++ src/controller/filme.controller.ts
@@ -4,61 +4,43 @@ import { AuthPayload } from "../tipos/auth-payload";
 
 const filmeService = new FilmeService();
 
-export class FilmeController{
+export class FilmeController {
+  async create(req: Request, res: Response) {
+    const user = res.locals.user as AuthPayload;
 
-    async create(req: Request,res: Response)
-    {
+    const dados = req.body;
+    await filmeService.create(dados, user.id);
+    res.status(200).send();
+  }
 
-        const user =
-            res.locals.user as AuthPayload;
+  
 
-        const dados = req.body;
-        await filmeService.create(dados,user.id);
-        res.status(200).send();
-    }
+async update(req: Request, res: Response) {
+  const user = res.locals.user as AuthPayload;
+  const id = Number(req.params.id);
+  const dados = req.body;
+  await filmeService.update(id, user.id, dados);
+  res.status(200).send();
+}
 
-    async delete(req: Request, res: Response)
-    {
-           const user =
-            res.locals.user as AuthPayload;
+async delete(req: Request, res: Response) {
+  const user = res.locals.user as AuthPayload;
+  const id = Number(req.params.id);
+  await filmeService.delete(id, user.id);
+  res.status(200).send();
+}
 
-        const id = Number(req.params.id);
-        await filmeService.delete(id,user.id);
-        res.status(200).send();
-    }
+  async findAll(req: Request, res: Response) {
+    const user = res.locals.user as AuthPayload;
 
-    async update(req:Request, res:Response)
-    {
+    const filmes = await filmeService.findAll(user.id);
+    res.status(200).json(filmes);
+  }
 
-        const user =
-            res.locals.user as AuthPayload;
-
-        const id = Number(req.params.id);
-        const dados = req.body;
-        await filmeService.update(id,user.id,dados);
-        res.status(200).send();
-    }
-
-    async findAll(req:Request, res:Response)
-    {
-        const user =
-            res.locals.user as AuthPayload;
-
-        const filmes = await filmeService.findAll(user.id);
-        res.status(200).json(filmes);
-    }
-
-    async findById(req:Request, res:Response)
-    {
-        
-        const user =
-            res.locals.user as AuthPayload;
-        const id = Number(req.params.id);
-        const filme = await filmeService.findById(id,user.id);
-        res.status(200).json(filme);
-    }
-
-
-    
-
+  async findById(req: Request, res: Response) {
+    const user = res.locals.user as AuthPayload;
+    const id = Number(req.params.id);
+    const filme = await filmeService.findById(id, user.id);
+    res.status(200).json(filme);
+  }
 }
