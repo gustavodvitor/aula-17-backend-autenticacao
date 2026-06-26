50cc7030da8eb5da4669c90117520f113db3793c
Gustavo Daniel Vitor <d2021020134@unifei.edu.br>
2026-06-26 19:22:09 -0300

    feat: implementa UPDATE e DELETE de filmes (rotas, controller e service)

--- src/services/filme.service.ts
+++ src/services/filme.service.ts
@@ -1,56 +1,49 @@
 import { prisma } from "../prisma/client";
 
-export class FilmeService{
+export class FilmeService {
+  async create(
+    dados: { titulo: string; nota: number; imagem: string },
+    userId: number,
+  ) {
+    await prisma.filme.create({
+      data: {
+        ...dados,
+        userId,
+      },
+    });
+  }
 
-    async create(dados: {titulo: string; nota: number; imagem: string;}, userId: number)
-    {
-        await prisma.filme.create({
-            data:{
-                ...dados,
-                userId,
-            },
-        });
-    }
+  async findAll(userId: number) {
+    return await prisma.filme.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
 
-    async delete(id: number, userId: number)
-    {
-        await prisma.filme.delete({
-            where: {
-                id,
-                userId
-            },
-        });
-    }
+  async findById(id: number, userId: number) {
+    return await prisma.filme.findUnique({
+      where: {
+        id,
+        userId,
+      },
+    });
+  }
 
-    async findAll(userId: number)
-    {
-        return await prisma.filme.findMany({
-            where: {
-                userId
-            }
-        });
-    }
+  async update(
+    id: number,
+    userId: number,
+    dados: { titulo?: string; genero?: string; nota?: number },
+  ) {
+    return prisma.filme.update({
+      where: { id, userId },
+      data: dados,
+    });
+  }
 
-    async findById(id: number, userId: number)
-    {
-        return await prisma.filme.findUnique({
-            where: {
-                id,
-                userId
-            }
-        });
-    }
-
-    async update(id:number, userId: number, dados: {titulo: string; nota: number; imagem: string; })
-    {
-
-        await prisma.filme.update({
-            where:{
-                id,
-                userId,
-            },
-            data: dados,
-        });
-    }
-
+  async delete(id: number, userId: number) {
+    return prisma.filme.delete({
+      where: { id, userId },
+    });
+  }
 }
